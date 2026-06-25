f568d3b5bc35350cdd7e72399aa667598269d8ea
padalakomali <padalakomali990@gmail.com>
2026-06-25 22:08:21 +0530

    formatted code

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,41 +7,41 @@ function Navbar() {
   const navigate = useNavigate();
 
   const location = useLocation();
-const [admin, setAdmin] = useState(null);
-const [user, setUser] = useState(null);
+  const [admin, setAdmin] = useState(null);
+  const [user, setUser] = useState(null);
 
-useEffect(() => {
-  const adminData = JSON.parse(localStorage.getItem("admin"));
-  const userData = JSON.parse(localStorage.getItem("user"));
+  useEffect(() => {
+    const adminData = JSON.parse(localStorage.getItem("admin"));
+    const userData = JSON.parse(localStorage.getItem("user"));
 
-  setAdmin(adminData);
-  setUser(userData);
-}, [location]);
+    setAdmin(adminData);
+    setUser(userData);
+  }, [location]);
 
-
+  
 
   async function logout() {
-  try {
-    if (admin) {
-      localStorage.removeItem("admin");
+    try {
+      if (admin) {
+        localStorage.removeItem("admin");
+      }
+
+      if (user) {
+        await axios.post(
+          "/api/user/logout",
+        );
+
+        localStorage.removeItem("user");
+      }
+
+      setAdmin(null);
+      setUser(null);
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
     }
-
-    if (user) {
-      await axios.post(
-        "/api/user/logout",
-      );
-
-      localStorage.removeItem("user");
-    }
-  
-    setAdmin(null);
-    setUser(null);
-
-    navigate("/login");
-  } catch (error) {
-    console.log(error);
   }
-}
 
   return (
     <nav className="navbar navbar-expand-lg custom-navbar">
@@ -64,189 +64,189 @@ useEffect(() => {
           </span>
         </button>
 
-       {/* NAVBAR LINKS */}
-<div
-  className="collapse navbar-collapse justify-content-end"
-  id="navbarSupportedContent"
->
-  <ul className="navbar-nav mb-2 mb-lg-0">
+        {/* NAVBAR LINKS */}
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav mb-2 mb-lg-0">
 
-    <li className="nav-item">
-      <Link className="nav-link custom-link" to="/">
-        Home
-      </Link>
-    </li>
-
-    {/* GUEST NAVBAR */}
-    {!admin && !user && (
-      <>
-        <li className="nav-item">
-          <Link className="nav-link custom-link" to="/login">
-            Login
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link className="nav-link custom-link" to="/register">
-            Register
-          </Link>
-        </li>
-      </>
-    )}
-
-    {/* ADMIN NAVBAR */}
-    {admin && (
-      <>
-        <li className="nav-item">
-          <Link className="nav-link custom-link" to="/dashboard">
-            Dashboard
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link className="nav-link custom-link" to="/products">
-            Products
-          </Link>
-        </li>
-
-        <li className="nav-item dropdown">
-          <span
-            className="nav-link dropdown-toggle custom-link"
-            role="button"
-            data-bs-toggle="dropdown"
-            style={{ cursor: "pointer" }}
-          >
-            Admin
-          </span>
-
-          <ul className="dropdown-menu">
-            <li>
-              <Link
-                className="dropdown-item"
-                to="/add-product"
-              >
-                Add Product
+            <li className="nav-item">
+              <Link className="nav-link custom-link" to="/">
+                Home
               </Link>
             </li>
 
-            <li>
-              <Link
-                className="dropdown-item"
-                to="/admin-products"
-              >
-                Admin Products
-              </Link>
-            </li>
+            {/* GUEST NAVBAR */}
+            {!admin && !user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link custom-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link custom-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
+
+            {/* ADMIN NAVBAR */}
+            {admin && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link custom-link" to="/dashboard">
+                    Dashboard
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link custom-link" to="/products">
+                    Products
+                  </Link>
+                </li>
+
+                <li className="nav-item dropdown">
+                  <span
+                    className="nav-link dropdown-toggle custom-link"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    style={{ cursor: "pointer" }}
+                  >
+                    Admin
+                  </span>
+
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/add-product"
+                      >
+                        Add Product
+                      </Link>
+                    </li>
+
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to="/admin-products"
+                      >
+                        Admin Products
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+
+                <li className="nav-item">
+                  <button
+                    className="logout-btn"
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            )}
+
+            {/* USER NAVBAR */}
+            {user && (
+              <>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link custom-link"
+                    to="/user-dashboard"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link custom-link"
+                    to="/produce"
+                  >
+                    Products
+                  </Link>
+                </li>
+
+                {/* CATEGORIES DROPDOWN */}
+                <li className="nav-item dropdown">
+                  <span
+                    className="nav-link dropdown-toggle custom-link"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    style={{ cursor: "pointer" }}
+                  >
+                    Categories
+                  </span>
+                  <ul className="dropdown-menu dropdown-menu-dark-custom">
+                    <li>
+                      <Link className="dropdown-item" to="/produce?category=home_appliences">
+                        🏠 Home Appliances
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/produce?category=Grocery">
+                        🛒 Grocery
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/produce?category=Fashion">
+                        👗 Fashion
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/produce?category=Electronics">
+                        📱 Electronics
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/produce?category=Sports">
+                        ⚽ Sports
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/produce?category=Toys">
+                        🧸 Toys
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link custom-link"
+                    to="/cart"
+                  >
+                    Cart
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link custom-link"
+                    to="/my-orders"
+                  >
+                    Orders
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <button
+                    className="logout-btn"
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            )}
+
           </ul>
-        </li>
-
-        <li className="nav-item">
-          <button
-            className="logout-btn"
-            onClick={logout}
-          >
-            Logout
-          </button>
-        </li>
-      </>
-    )}
-
-    {/* USER NAVBAR */}
-    {user && (
-      <>
-        <li className="nav-item">
-          <Link
-            className="nav-link custom-link"
-            to="/user-dashboard"
-          >
-            Dashboard
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            className="nav-link custom-link"
-            to="/produce"
-          >
-            Products
-          </Link>
-        </li>
-
-        {/* CATEGORIES DROPDOWN */}
-        <li className="nav-item dropdown">
-          <span
-            className="nav-link dropdown-toggle custom-link"
-            role="button"
-            data-bs-toggle="dropdown"
-            style={{ cursor: "pointer" }}
-          >
-            Categories
-          </span>
-          <ul className="dropdown-menu dropdown-menu-dark-custom">
-            <li>
-              <Link className="dropdown-item" to="/produce?category=home_appliences">
-                🏠 Home Appliances
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/produce?category=Grocery">
-                🛒 Grocery
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/produce?category=Fashion">
-                👗 Fashion
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/produce?category=Electronics">
-                📱 Electronics
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/produce?category=Sports">
-                ⚽ Sports
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/produce?category=Toys">
-                🧸 Toys
-              </Link>
-            </li>
-          </ul>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            className="nav-link custom-link"
-            to="/cart"
-          >
-            Cart
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            className="nav-link custom-link"
-            to="/my-orders"
-          >
-            Orders
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <button
-            className="logout-btn"
-            onClick={logout}
-          >
-            Logout
-          </button>
-        </li>
-      </>
-    )}
-
-  </ul>
-</div>
+        </div>
       </div>
     </nav>
   );
