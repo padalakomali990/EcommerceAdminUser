8efdc90abd774f412032cb0bda430e3e61dc39d6
padalakomali <padalakomali990@gmail.com>
2026-06-26 20:22:56 +0530

    credits

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
-// src/api.js
-
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://kcommerce.duckdns.org",
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
+
+console.log("API BASE URL =", api.defaults.baseURL);
 
 export default api;
